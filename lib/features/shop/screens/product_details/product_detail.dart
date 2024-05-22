68f46f96f1e62d8f27a3aee70ef144a998bdb623
BariqAbrarRamadhan <bariqrama02@gmail.com>
2024-05-23 01:35:46 +0700

    Product Detail v0.2

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
+import 'package:tapu_tapi_shop/common/widgets/texts/section_heading.dart';
+import 'package:tapu_tapi_shop/features/shop/screens/product_details/widgets/bottom_add_to_cart.dart';
+import 'package:tapu_tapi_shop/features/shop/screens/product_details/widgets/product_attributes.dart';
 import 'package:tapu_tapi_shop/features/shop/screens/product_details/widgets/product_image_slider.dart';
 import 'package:tapu_tapi_shop/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:tapu_tapi_shop/features/shop/screens/product_details/widgets/rating_share_widget.dart';
@@ -11,18 +16,54 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TProductImageSlider(),
-
+            const TProductImageSlider(),
             Padding(
-              padding: EdgeInsets.only(right: TSizes.defaultSpace, left: TSizes.defaultSpace, bottom: TSizes.defaultSpace),
+              padding: const EdgeInsets.only(
+                  right: TSizes.defaultSpace,
+                  left: TSizes.defaultSpace,
+                  bottom: TSizes.defaultSpace),
               child: Column(
                 children: [
-                  TRatingAndShare(),
-                  TProductMetaData()
+                  const TRatingAndShare(),
+                  const TProductMetaData(),
+                  const TProductAttributes(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text('Checkout'))),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                  const TSectionHeading(
+                      title: 'Description', showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const ReadMoreText(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. sdasias dfdusahd chsaidhiashuhasiduuasfiuca sfc asoiu fc assuch as',
+                      trimLines: 2,
+                      trimMode: TrimMode.Line,
+                      trimCollapsedText: 'Show more',
+                      trimExpandedText: 'Less',
+                      moreStyle:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                      lessStyle:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w800)),
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(
+                          title: 'Reviews (199)', showActionButton: false),
+                      IconButton(
+                          icon: const Icon(Iconsax.arrow_right_3, size: 18),
+                          onPressed: () {}),
+                    ],
+                  ),
+                  const SizedBox(height: TSizes.spaceBtwSections),
                 ],
               ),
             )
